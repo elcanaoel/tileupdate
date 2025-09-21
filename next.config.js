d78bd96759f4ef8dd5ecd9dc8c102f8fd54f39d1
Baouldguyd <damilolamiraclek@gmail.com>
2025-09-21 16:02:54 +0100

    main

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Enable TypeScript support
-  typescript: {
-    // Ignore type errors during build (temporary solution)
-    ignoreBuildErrors: true,
-  },
-  // Enable ESLint support
-  eslint: {
-    // Ignore ESLint errors during build (temporary solution)
-    ignoreDuringBuilds: true,
-  },
+  // // Enable TypeScript support
+  // typescript: {
+  //   // Ignore type errors during build (temporary solution)
+  //   ignoreBuildErrors: true,
+  // },
+  // // Enable ESLint support
+  // eslint: {
+  //   // Ignore ESLint errors during build (temporary solution)
+  //   ignoreDuringBuilds: true,
+  // },
   images: {
     domains: ['images.unsplash.com', ],
   }
